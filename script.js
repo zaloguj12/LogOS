636d1f80b8d27b11a34080fb9ec748cccb18b643
Zaloguj12 <zaloguj12@proton.me>
2026-07-14 18:24:49 +0200

    try to add a TWM like function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let windows = [];
+
 function enter() {
     const welcontent = document.getElementById("welContent");
     const maincontent = document.getElementById("mainContent")
@@ -13,4 +15,84 @@ function enter() {
     setTimeout(() => {
         welcontent.style.display = "none";
     }, 500);
+}
+
+let openedApps = [];
+
+
+function openApp(id){
+
+    let app=document.getElementById(id);
+
+
+    if(!openedApps.includes(app)){
+        openedApps.push(app);
+    }
+
+
+    app.style.display="block";
+
+
+    renderWindows();
+
+
+    focusWindow(app);
+}
+
+
+
+function closeApp(id){
+
+    let app=document.getElementById(id);
+
+
+    app.style.display="none";
+
+
+    openedApps =
+        openedApps.filter(x=>x!==app);
+
+
+    renderWindows();
+}
+
+
+
+function renderWindows() {
+    let workspace=document.getElementById("workspace");
+    workspace.innerHTML = "";
+    openedApps.forEach(app => {
+        workspace.appendChild(app);
+    });
+
+    if (openedApps.length === 1) {
+        openedApps[0].style.width = "100%";
+    }
+    else if (openedApps.length >= 2) {
+        openedApps.forEach((app, index) => {
+            if (index === 0) {
+                app.style.width = "35%";
+            }
+            else {
+                app.style.width = "65%";
+            }
+        });
+    }
+}
+
+function focusWindow(app) {
+    document.querySelectorAll(".appWindow").forEach(w => w.classList.remove("active"));
+    app.classList.add("active");
+}
+
+document.addEventListener("click", e => {
+    let win = e.target.closest(".appWindow");
+    if (win)
+        focusWindow(win);
+});
+
+function focusWindow(app) {
+    document.querySelectorAll(".appWindow").forEach(w => w.classList.remove("active"));
+    app.classList.add("active");
+    app.style.order = windows.indexOf(app);
 }
